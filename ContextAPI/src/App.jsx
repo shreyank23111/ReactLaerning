abdc53c2744d49d2ba4446878d16d240f47ad91a
Shreyank Desai <shreyankdessai6@gmail.com>
2023-12-29 08:33:20 +0530

    mini Project1 with context API

---
+++ ContextAPI/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Login from './Component/Login'
+import Profile from './Component/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+    
+  )
+}
+
+export default App
